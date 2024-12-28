9a92b67497d42dbd28877f99723d645131425a76
GhaLuccas <gitsgab@gmail.com>
2024-12-28 11:24:28 -0300

    Teorica da web - aula 02 , exercicios concluidos

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -12,3 +12,12 @@ def test_read_root_deve_retornar_OK_e_helloword():
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'hello word'}  # Assert
+
+
+def test_read_html_deve_retornar_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert ' <h1> Olá Mundo </h1>' in response.text
