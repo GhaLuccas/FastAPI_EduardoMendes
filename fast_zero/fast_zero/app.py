8367bd9f4b978ad15413ca98931a297b61d0f966
GhaLuccas <gitsgab@gmail.com>
2024-12-29 14:02:27 -0300

    Operaçoes CRUD e o uso do response_model junto com a criação de Schemas - aula 03

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -3,9 +3,10 @@ from http import HTTPStatus
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserSchema  , UserPublic
 
 app = FastAPI()
+
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -24,3 +25,8 @@ def read_html():
         <h1> Olá Mundo </h1>
       </body>
     </html>"""
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
